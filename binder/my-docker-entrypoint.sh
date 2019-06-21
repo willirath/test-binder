3d33d4e73ae4f04388acea0e080fca950c2369ba
Willi Rath <wrath@geomar.de>
2019-06-21 10:37:10 +0200

    Adapt paths to binder/ and explicitly add slurm config

--- binder/my-docker-entrypoint.sh
+++ binder/my-docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo sed -i.bak -e "s@ernie@localhost@" /etc/slurm/slurm.conf
-sudo /usr/local/bin/docker-entrypoint.sh
 sudo chown ${USER}:${USER} ${HOME}/*ipynb
+sudo /usr/local/bin/docker-entrypoint.sh
 
 exec "$@"
